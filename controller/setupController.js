641171caf7d6f19844c643e8be89038c33783f78
Abhishek Hazra <abhishek.hazra@awesomesuite.com>
2023-07-15 16:15:35 +0530

    Admin Add At Property Add Time

--- controller/setupController.js
+++ controller/setupController.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const PropertyDetails = require("../models/property");
 const Floor = require("../models/floor");
 const Rooms = require("../models/rooms");
+const Member = require("../models/member");
+const moment = require("moment");
 const roomAddHelper = require("../helper/roomAddHelper");
 
 const addProperty = (req, res) => {
@@ -9,16 +11,56 @@ const addProperty = (req, res) => {
   const newProperty = new PropertyDetails({ userId, name, contact, pincode });
   newProperty
     .save()
-    .then(() =>
-      res.json({
-        code: 200,
-        message: "Property Added",
+    .then(() => {
+      const newMember = new Member({
         userId,
         propertyId: newProperty._id,
-        propertyName: name,
-      })
-    )
-    .catch((err) => res.status(400).json({ code: 200, message: err.message }));
+        phone: contact,
+        personal: {
+          name: "Owner",
+          phone: contact,
+          doj: moment().format("DD-MM-YYYY"),
+          designation: "Admin",
+        },
+        kyc: {},
+        current: {
+          address: "",
+          pincode: "",
+          city: "",
+          state: "",
+        },
+        permanent: {
+          address: "",
+          pincode: "",
+          city: "",
+          state: "",
+        },
+        job: {},
+        bank: {
+          account: "",
+          name: "",
+          ifsc: "",
+          holder: "",
+        },
+      });
+      newMember
+        .save()
+        .then(() => {
+          return res.json({
+            code: 200,
+            message: "Property Added",
+            userId,
+            propertyId: newProperty._id,
+            propertyName: name,
+          });
+        })
+        .catch((err) => {
+          return res.json({ code: 502, model: err.message });
+        });
+    })
+    .catch((err) => {
+      return res.status(400).json({ code: 200, message: err.message });
+    });
 };
 
 const addFloor = (req, res) => {
