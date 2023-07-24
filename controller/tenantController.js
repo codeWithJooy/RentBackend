79dd470abb37567efcbba7bbdde8505deb36b168
Abhishek Hazra <abhishek.hazra@awesomesuite.com>
2023-07-24 21:55:16 +0530

    Tenant Searcg Backend

--- controller/tenantController.js
+++ controller/tenantController.js
@@ -3,6 +3,7 @@ const Tenant = require("../models/tenant");
 const Collection = require("../models/collection");
 const Discount = require("../models/discount");
 const Receipt = require("../models/receipt");
+const Rooms = require("../models/rooms");
 
 const addTenant = (req, res) => {
   let {
@@ -98,15 +99,40 @@ const addTenant = (req, res) => {
 };
 const getTenants = (req, res) => {
   const { userId, propertyId } = req.query;
-  Tenant.find({ userId, propertyId })
-    .then((docs) => {
-      if (!docs) {
-        return res.json({ code: 404 });
-      }
-      return res.json({ code: 200, model: docs });
+  Rooms.find({ userId, propertyId })
+    .exec()
+    .then((rooms) => {
+      const allRooms = rooms.reduce((acc, curr) => [...acc, ...curr.rooms], []);
+      Tenant.find({ userId, propertyId })
+        .exec()
+        .then((docs) => {
+          if (!docs) {
+            return res.json({ code: 404 });
+          }
+          let arr = [];
+          for (let i = 0; i < docs.length; i++) {
+            const room = allRooms.filter((item) => item.id == docs[i].roomId);
+            let obj = {};
+            obj._id = docs[i]._id;
+            obj.userId = docs[i].userId;
+            obj.propertyId = docs[i].propertyId;
+            obj.roomId = docs[i].roomId;
+            obj.name = docs[i].name;
+            obj.number = docs[i].number;
+            obj.roomName = room[0].name.toString();
+            obj.dob = docs[0].dob;
+            obj.dues = docs[i].dues;
+            arr.push(obj);
+          }
+          return res.json({ code: 200, model: arr });
+        })
+        .catch((err) => {
+          return res.json({ code: 502, model: err.message });
+        });
     })
     .catch((err) => {
-      return res.json({ code: 502 });
+      console.log(err);
+      res.json({ code: 502 });
     });
 };
 const getATenant = (req, res) => {
