db03e67d398e6ab612d73d87fe2a634864003eff
Abhishek Hazra <abhishek.hazra@awesomesuite.com>
2023-07-29 12:21:27 +0530

    Student Route Updated

--- routes/Student/studentRoutes.js
+++ routes/Student/studentRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const studentController = require("../../controller/Student/studentController");
 
 router.get("/checkCodeNumber", studentController.checkCodeNumber);
+router.post("/addStudent", studentController.addStudent);
 
 module.exports = router;
