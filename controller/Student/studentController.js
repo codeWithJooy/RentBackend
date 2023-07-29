6932e172824ff44f5fcbc1428e44ee3470e09245
Abhishek Hazra <abhishek.hazra@awesomesuite.com>
2023-07-29 12:21:40 +0530

    Student Controller Updated

--- controller/Student/studentController.js
+++ controller/Student/studentController.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Property = require("../../models/property");
 const Tenant = require("../../models/tenant");
+const Student = require("../../models/student");
 
 const checkCodeNumber = (req, res) => {
   const { code, number } = req.query;
@@ -9,12 +10,14 @@ const checkCodeNumber = (req, res) => {
       if (property) {
         const userId = property.userId;
         const propertyId = property._id;
+        const propertyName = property.name;
         Tenant.findOne({ userId, propertyId, number })
           .then((tenant) => {
             if (tenant) {
               let obj = {};
               obj.userId = userId;
               obj.propertyId = propertyId;
+              obj.propertyName = propertyName;
               obj.name = tenant.name;
               obj.tenantId = tenant._id;
               obj.number = number;
@@ -34,7 +37,49 @@ const checkCodeNumber = (req, res) => {
       return res.json({ code: 502, model: err.message });
     });
 };
+const addStudent = (req, res) => {
+  const {
+    userId,
+    propertyId,
+    propertyName,
+    tenantId,
+    email,
+    name,
+    number,
+    password,
+  } = req.body;
+
+  Student.findOne({ number })
+    .then((student) => {
+      if (student) {
+        return res.json({ code: 409, model: "Tenant Already Present" });
+      } else {
+        let newStudent = new Student({
+          userId,
+          propertyId,
+          propertyName,
+          tenantId,
+          email,
+          number,
+          name,
+          password,
+        });
+        newStudent
+          .save()
+          .then(() => {
+            return res.json({ code: 200, model: newStudent });
+          })
+          .catch((err) => {
+            return res.json({ code: 502, model: err.message });
+          });
+      }
+    })
+    .catch((err) => {
+      return res.json({ code: 502, model: err.message });
+    });
+};
 
 module.exports = {
   checkCodeNumber,
+  addStudent,
 };
