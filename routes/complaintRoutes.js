e4e7ae8af72e157aea3d125bd79d240fb1333a0a
Abhishek Hazra <abhishek.hazra@awesomesuite.com>
2023-08-01 23:37:16 +0530

    Complaint Route and Test

--- routes/complaintRoutes.js
+++ routes/complaintRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router()
 const complaintController = require("../controller/complaintController")
 
 router.post("/raiseComplaint", complaintController.raiseComplaint)
-
+router.get("/getComplaints",complaintController.getComplaints)
 module.exports=router
