3da1d29420cf4e8a9be3135fb0e0c75853262394
Abhishek Hazra <abhishek.hazra@awesomesuite.com>
2023-08-19 07:34:46 +0530

    Dues Route Updated

--- routes/dueRoutes.js
+++ routes/dueRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const dueControllers = require("../controller/duesController");
 
+
 router.post("/addDuesTenant", dueControllers.addDuesTenant);
 router.post("/addDuesRoom", dueControllers.addDuesRoom);
-
+router.get("/getDuesTenant", dueControllers.getDuesTenant)
 module.exports = router;
