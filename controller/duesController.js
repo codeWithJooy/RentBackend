b4dd32282f6c363a75522b7eaf8036d47915af68
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-08-22 14:02:15 +0530

    Changes In duesController

--- controller/duesController.js
+++ controller/duesController.js
@@ -2,46 +2,51 @@ const mongoose = require("mongoose");
 const Tenant = require("../models/tenant");
 const Dues = require("../models/dues")
 
-const addDuesRoom = (req, res) => {
-  const { userId, propertyId, roomId } = req.query;
-  const data = req.body;
+const addDuesRoom = async (req, res) => {
+  try {
+    const { userId, propertyId, roomId } = req.query;
+    const { type, rent, total, due, collection, discount, description, dueDate } = req.body
 
-  Tenant.find({ userId, propertyId, roomId })
-    .exec()
-    .then((tenants) => {
-      if (tenants) {
-        for (let i = 0; i < tenants.length; i++) {
-          tenants[i].dues.push(data);
-          tenants[i].markModified("dues");
-          tenants[i].save();
+    let tenants = await Tenant.find({ userId, propertyId, roomId }).exec()
+    if (tenants) {
+      for (let i = 0; i < tenants.length; i++) {
+        let duePresent = await Dues.findOne({ userId, propertyId, tenantId: tenants[i]._id, dueType: type }).exec()
+        if (duePresent) {
+          if (duePresent.due - duePresent.collections > 0) {
+            return res.send({ code: 404, msg: "DueType Already Present" })
+          }
         }
-        return res.json({ code: 200, model: "Updated" });
-      } else {
-        return res.json({ code: 404, model: "Not Found" });
-      }
-    })
-    .catch((err) => {
-      return res.json({ code: 502, model: err.message });
-    });
-};
-const addDuesTenant = (req, res) => {
-  const { userId, propertyId, tenantId } = req.query;
-  const data = req.body;
 
-  Tenant.findOne({ userId, propertyId, _id: tenantId })
-    .then((tenant) => {
-      if (tenant) {
-        tenant.dues.push(data);
-        tenant.markModified("dues");
-        tenant.save();
-        return res.json({ code: 200, model: "Updated" });
-      } else {
-        return res.json({ code: 404, model: "Not Found" });
+        let dueData = new Dues({ userId, propertyId, tenantId: tenants[i]._id, dueType: type, rent, total, due, collections: collection, discount, description, dueDate }).save()
       }
-    })
-    .catch((err) => {
-      return res.json({ code: 502, model: err.message });
-    });
+      return res.json({ code: 200 })
+    }
+    else {
+      return res.send({ code: 404, msg: "No Tenant Present" })
+    }
+  }
+  catch (error) {
+    return res.json({ code: 502, msg: error.message })
+  }
+};
+const addDuesTenant = async (req, res) => {
+  try {
+    const { userId, propertyId, tenantId } = req.query;
+    const { type, rent, total, due, collection, discount, description, dueDate } = req.body
+    //Check If Due type present and due Remaining for the type is greater then 0
+    let duePresent = await Dues.findOne({ userId, propertyId, tenantId, dueType: type }).exec()
+    if (duePresent) {
+      if (duePresent.due - duePresent.collections > 0) {
+        return res.send({ code: 404, msg: "DueType Already Present" })
+      }
+    }
+
+    let dueData = new Dues({ userId, propertyId, tenantId, dueType: type, rent, total, due, collections: collection, discount, description, dueDate }).save()
+
+    return res.json({ code: 200 })
+  } catch (error) {
+    return res.json({ code: 502, msg: error.message })
+  }
 };
 const getDuesTenant = async (req, res) => {
   try {
