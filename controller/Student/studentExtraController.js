e5d1bb0e02ba05a512a700630a68a5c0a3b26074
Abhishek <enigma.abhi.ty@gmail.com.com>
2023-09-09 22:09:01 +0530

    Done Hosting

--- controller/Student/studentExtraController.js
+++ controller/Student/studentExtraController.js
@@ -2,14 +2,22 @@ const mongoose = require("mongoose")
 const Eviction = require("../../models/studentExtra/eviction")
 const Hosting = require("../../models/studentExtra/hosting")
 const Late = require("../../models/studentExtra/late")
+const StudentNotifications=require("../../models/studentExtra/studentNotifications")
 
 const addStudentHosting = async (req, res) => {
     try {
         const { userId, propertyId, tenantId } = req.query
-        const { name, from, to } = req.body
-
-        let
-
+        const { name,phone, from, to,presentDate } = req.body
+        let message=`Request for Hosting ${name} from ${from} to ${to} send.`
+        let present=Hosting.findOne({userId,propertyId,tenantId,status:"Pending"}).exec()
+        if(!present){
+            let hosting=await new Hosting({userId,propertyId,tenantId,name,phone,from,to}).save()
+        let notification=await new StudentNotifications({userId,propertyId,tenantId,date:presentDate,message}).save()
+        return res.json({code:200,msg:"Request Send"})
+        }
+        else{
+            return res.json({code:404,msg:"Earlier Reuest Still Pending "})
+        }
     }
     catch (error) {
         return res.json({ code: 502, msg: error.message })
@@ -30,10 +38,11 @@ const addStudentEviction = async (req, res) => {
 const addStudentLate = async (req, res) => {
     try {
         const { userId, propertyId, tenantId } = req.query
-        const { name, from, to } = req.body
-
-        let
-
+        const { date,time,presentDate } = req.body
+        let message=`Request for late arrival on ${date} by time ${time}`
+        let lete=new Late({userId,propertyId,tenantId,date,time}).save()
+        let notification=new StudentNotification({userId,propertyId,tenantId,date:presentDate,message})
+        return res.json({code:200,msg:"Request Send"})
     }
     catch (error) {
         return res.json({ code: 502, msg: error.message })
@@ -42,5 +51,5 @@ const addStudentLate = async (req, res) => {
 module.exports = {
     addStudentHosting,
     addStudentEviction,
-    addStudentHosting,
+    addStudentLate,
 }
--- routes/Student/studentRoutes.js
+++ routes/Student/studentRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const studentController = require("../../controller/Student/studentController");
-
+const studentExtraController=require("../../controller/Student/studentExtraController");
 router.get("/checkCodeNumber", studentController.checkCodeNumber);
 router.post("/addStudent", studentController.addStudent);
 router.get("/studentLogin", studentController.studentLogin);
@@ -11,4 +11,7 @@ router.get("/getStudentDuesStatus", studentController.getStudentDuesStatus)
 router.get("/getStudentTotalDues", studentController.getStudentTotalDues)
 router.get("/getStudentTotalExpenses", studentController.getStudentTotalExpenses)
 router.get("/getStudentExpenses", studentController.getStudentExpenses)
+router.post("/addStudentLate",studentExtraController.addStudentLate)
+router.post("/addStudentHosting",studentExtraController.addStudentHosting)
+router.post("/addStudentEviction",studentExtraController.addStudentEviction)
 module.exports = router;
