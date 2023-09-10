52c83290f82f93487d848c9e419f10382de5250c
Abhishek <enigma.abhi.ty@gmail.com.com>
2023-09-10 06:59:14 +0530

    Expense

--- controller/expenseController.js
+++ controller/expenseController.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Expense = require("../models/expense");
-const Member=require("../models/member")
+const Member = require("../models/member")
 
 const addExpense = (req, res) => {
   const {
@@ -39,39 +39,72 @@ const getExpense = (req, res) => {
   Expense.find({ userId, propertyId }).exec()
     .then((doc) => {
       if (!doc) {
-        return res.json({ code: 404,msg:"No Expense Found" });
+        return res.json({ code: 404, msg: "No Expense Found" });
       } else {
         return res.json({ code: 200, model: doc });
       }
     })
     .catch((err) => {
-      return res.json({ code: 502 ,msg:err.message});
+      return res.json({ code: 502, msg: err.message });
     });
 };
-const getMemberName=async(req,res)=>{
-  try{
-     const {userId,propertyId}=req.query
-     
-     let member=await Member.find({userId,propertyId}).exec()
-     let arr=[]
-     if(member){
+const getMemberName = async (req, res) => {
+  try {
+    const { userId, propertyId } = req.query
+
+    let member = await Member.find({ userId, propertyId }).exec()
+    let arr = []
+    if (member) {
       console.log(member.length)
-      for(let i= 0;i<member.length;i++){
-         arr.push(member[i].personal.name)
+      for (let i = 0; i < member.length; i++) {
+        arr.push(member[i].personal.name)
       }
-      return res.json({code:200,model:arr})
-     }
-     else{
-      return res.json({code:200,model:arr})
-     }
-     
+      return res.json({ code: 200, model: arr })
+    }
+    else {
+      return res.json({ code: 200, model: arr })
+    }
+
   }
-  catch(error){
-    return res.json({code:502,msg:error.message})
+  catch (error) {
+    return res.json({ code: 502, msg: error.message })
+  }
+}
+const getExpenseCount = async (req, res) => {
+  try {
+    let { userId, propertyId } = req.query
+    let expense = await Expense.find({ userId, propertyId }).exec()
+    if (expense) {
+      return res.json({ code: 200, model: expense.length })
+    }
+    else {
+      return res.json({ code: 200, model: 0 })
+    }
+  }
+  catch (error) {
+    return res.json({ code: 502, msg:error.message })
+  }
+}
+const getTotalExpense = async (req, res) => {
+  try {
+    let { userId, propertyId } = req.query
+    let expense = await Expense.find({ userId, propertyId }).exec()
+    if (expense) {
+      let totalExpense = expense.reduce((acc, curr) => acc + parseInt(curr.amount), 0)
+      return res.json({ code: 200, model: totalExpense })
+    }
+    else {
+      return res.json({ code: 200, model: 0 })
+    }
+  }
+  catch (error) {
+    return res.json({ code: 502, msg:error.message })
   }
 }
 module.exports = {
   addExpense,
   getExpense,
   getMemberName,
+  getTotalExpense,
+  getExpenseCount,
 };
--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -5,4 +5,6 @@ const expenseController = require("../controller/expenseController");
 router.post("/addExpense", expenseController.addExpense);
 router.get("/getExpense", expenseController.getExpense);
 router.get("/getMemName",expenseController.getMemberName)
+router.get("/getTotalExpense",expenseController.getTotalExpense)
+router.get("/getExpenseCount",expenseController.getExpenseCount)
 module.exports = router;
